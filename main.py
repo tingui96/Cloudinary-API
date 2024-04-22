9c518b951213afc4d658435e1d48a168c39693d4
Yandy Sanchez Orosa <skullcandy961205@gmail.com>
2024-04-22 12:53:32 -0400

    add cors

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cloudinary.uploader
 import cloudinary.api
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -24,6 +25,16 @@ def up_image(url,name):
 
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def initial():
